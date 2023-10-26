1b7031a5b708a08bf80af69da9b951c7ff1873a6
Atanes <aatanes100@gmail.com>
2023-10-26 18:25:04 -0300

    Finalização do Desafio

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,7 @@
-CREATE TABLE People(
-       people_id INT PRIMARY KEY AUTO_INCREMENT, 
-       people_name VARCHAR(60)
+CREATE TABLE people(
+       person_id INT PRIMARY KEY AUTO_INCREMENT, 
+       person_name VARCHAR(60)
 );
-INSERT INTO People(people_name) VALUES("Atanes");
+INSERT INTO people(person_name) VALUES("Alexandre Jesus");
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password'; 
 flush privileges;
--- node/index.js
+++ node/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
-const port = 3000;
+
+const port = 5000;
 const config = {
-	host: 'db',
+	host: 'desafio-db',
 	user: 'root',
-	password: 'patanes',
+	password: 'password',
 	database: 'nodedb',
 };
 
-const mysql = require('mysql');
 const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Atanes')`;
+const sql = `INSERT INTO people(person_name) values('Atanes')`;
 const select = `SELECT * FROM people`;
 let people = [];
 connection.query(sql);
@@ -20,7 +21,7 @@ connection.end();
 
 app.get('/', (req, res) => {
 	let html = '<h1>Full Cycle Rocks!</h1>';
-	people.forEach((person) => html += `<p> - ${person.name}</p>`);
+	people.forEach((person) => html += `<p> - ${person.person_name}</p>`);
 	res.send(html);
 });
 
